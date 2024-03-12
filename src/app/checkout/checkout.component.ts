bbebff7d2f5fd1a83a889a67c385fe65fd1b1db8
Vikash <vikkusingh900@gmail.com>
2024-03-12 14:36:12 +0530

    updatd

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -6,29 +6,33 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css'],
+  styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+
   totalPrice: number | undefined;
   cartData: cart[] | undefined;
   orderMsg: string | undefined;
-  constructor(private product: ProductService, private router: Router) {}
+  constructor(private product: ProductService, private router: Router) { }
 
   ngOnInit(): void {
     this.product.currentCart().subscribe((result) => {
+
       let price = 0;
       this.cartData = result;
       result.forEach((item) => {
         if (item.quantity) {
-          price = price + +item.price * +item.quantity;
+          price = price + (+item.price * +item.quantity)
         }
-      });
-      this.totalPrice = price + price / 10 + 100 - price / 10;
+      })
+      this.totalPrice = price + (price / 10) + 100 - (price / 10);
 
       console.warn(this.totalPrice);
-    });
+
+    })
+
   }
-  orderNow(data: { email: string; address: string; contact: string }) {
+  orderNow(data: { email: string, address: string, contact: string }) {
     let user = localStorage.getItem('user');
     let userId = user && JSON.parse(user).id;
     if (this.totalPrice) {
@@ -36,24 +40,28 @@ export class CheckoutComponent implements OnInit {
         ...data,
         totalPrice: this.totalPrice,
         userId,
-        id: undefined,
-      };
+        id: undefined
+      }
 
       this.cartData?.forEach((item) => {
         setTimeout(() => {
           item.id && this.product.deleteCartItems(item.id);
-        }, 700);
-      });
+        }, 700)
+      })
 
       this.product.orderNow(orderData).subscribe((result) => {
         if (result) {
-          this.orderMsg = 'Order has been placed';
+          this.orderMsg = "Order has been placed";
           setTimeout(() => {
             this.orderMsg = undefined;
-            this.router.navigate(['/my-orders']);
+            this.router.navigate(['/my-orders'])
           }, 4000);
+
         }
-      });
+
+      })
     }
+
   }
+
 }
